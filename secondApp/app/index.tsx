e71913a68900f3238855347feaf90036eb9eaf53
nunna8021 <nikhil.nunna9@gmail.com>
2025-01-10 08:09:37 -0600

    Updated index.tsx
    Learned how to move object around the screen and align items

--- secondApp/app/index.tsx
+++ secondApp/app/index.tsx
@@ -4,8 +4,17 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Open app/index.tsx</Text>
+      <Text style={styles.header}>Ox</Text>
       <Text style={styles.textbody}>Hi</Text>
+      <View style={styles.textbodycontainer}>
+        <Text>A</Text>
+      </View>
+      <View style={styles.textbodycontainer}>
+        <Text>B</Text>
+      </View>
+      <View style={styles.textbodycontainer}>
+        <Text>C</Text>
+      </View>
     </View>
   );
 }
@@ -13,18 +22,32 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 15,
+    flex: 1, //fills whole space or ratio to other elements
+    padding: 150,
     margin: 20,
     backgroundColor: "orange",
-    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center", //main direction
+    alignItems: "center", // other direction
   },
   header: {
     fontSize: 20,
     fontWeight: "bold",
+    margin: 10,
   },
   textbody: {
-    fontSize: 18,
+    fontSize: 18
+  },
+  textbodycontainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: "yellow",
+    marginTop: 10,
+    justifyContent: "center",
+    margin: 10,
+    borderRadius: 25, //half of height width is a circle
+    borderColor: "green",
+    borderWidth: 2,
 
   },
 });

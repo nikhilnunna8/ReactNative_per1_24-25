e37b164bc1eebbfef73ecf1c165fee02bf259b6f
nunna8021 <nikhil.nunna9@gmail.com>
2025-01-14 09:24:21 -0600

    Updated assignmentOne
    added a rowcontainer so objects inside are next to each other

--- assignmentOne/app/(tabs)/index.tsx
+++ assignmentOne/app/(tabs)/index.tsx
@@ -10,7 +10,12 @@ export default function Index() {
       {/* Blue Container */}
       <View style={styles.blueContainer}>
         {/* Purple Circle 1 */}
-        <Text> 1 </Text>
+        <View style={styles.rowContainer}>
+          <Text style={styles.purpleCircle1}> 1 </Text> <View style={styles.greensquarebold}></View>
+        </View>
+        <View style={styles.rowContainer}>
+          <Text style={styles.purpleCircle1}> 1 </Text> <View style={styles.greensquarebold}></View>
+        </View>
       </View>
     </View>
   );
@@ -37,16 +42,34 @@ const styles = StyleSheet.create({
     padding: 15,
     margin: 20,
     backgroundColor: "lightblue",
-    alignItems: "center",
+    alignItems: "flex-start",
   },
   purpleCircle1: {
     height: 50,
     width: 50,
     backgroundColor: "purple",
-    justifyContent: "center",
+    alignContent: "center",
     padding: 10,
     borderRadius: 25, //half of height width is a circle
     borderColor: "black",
     borderWidth: 2,
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold",
+  },
+  greensquarebold: {
+    height: 50,
+    width: 100,
+    backgroundColor: "green",
+    alignContent: "center",
+    padding: 10,
+    borderColor: "darkgreen",
+    borderWidth: 2,
+    marginLeft: 20,
+  },
+  rowContainer: {
+    flexDirection: "row",
+    alignItems: "center", 
+    flex: 1,
   },
 });

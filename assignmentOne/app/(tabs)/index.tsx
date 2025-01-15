5ab1b398bc1a4eb1dc08226170464ec20ee25312
nunna8021 <nikhil.nunna9@gmail.com>
2025-01-15 08:11:40 -0600

    Added to firstApp and finished up AssignmentOne
    Worked with touchables and learned how to work with TouchableOpacity and highlight

--- assignmentOne/app/(tabs)/index.tsx
+++ assignmentOne/app/(tabs)/index.tsx
@@ -9,12 +9,15 @@ export default function Index() {
 
       {/* Blue Container */}
       <View style={styles.blueContainer}>
-        {/* Purple Circle 1 */}
-        <View style={styles.rowContainer}>
-          <Text style={styles.purpleCircle1}> 1 </Text> <View style={styles.greensquarebold}></View>
-        </View>
-        <View style={styles.rowContainer}>
-          <Text style={styles.purpleCircle1}> 1 </Text> <View style={styles.greensquarebold}></View>
+        <View>
+          {/* Purple Circle 1 and green box*/}
+          <View style={styles.rowContainer}>
+            <Text style={styles.purpleCircle1}> 1 </Text> <View style={styles.greensquarebold}></View>
+          </View>
+          {/* Purple Circle 2 and orange box*/}
+          <View style={styles.rowContainer}>
+            <Text style={styles.purpleCircle2}> 2 </Text> <View style={styles.orangesquarebold}></View>
+          </View>
         </View>
       </View>
     </View>
@@ -70,6 +73,29 @@ const styles = StyleSheet.create({
   rowContainer: {
     flexDirection: "row",
     alignItems: "center", 
-    flex: 1,
+    marginBottom: 15,
+  },
+  purpleCircle2: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
+    alignContent: "center",
+    padding: 10,
+    borderRadius: 25, //half of height width is a circle
+    borderColor: "Green",
+    borderWidth: 3,
+    fontSize: 22,
+    color: "green",
+    fontWeight: "bold",
+  },
+  orangesquarebold: {
+    height: 50,
+    width: 100,
+    backgroundColor: "orange",
+    alignContent: "center",
+    padding: 10,
+    borderColor: "red",
+    borderWidth: 2,
+    marginLeft: 20,
   },
 });
--- firstApp/app/(tabs)/index.tsx
+++ firstApp/app/(tabs)/index.tsx
@@ -1,74 +1,45 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Alert, Button, StyleSheet, Text, Touchable, TouchableHighlight, TouchableOpacity, View } from "react-native";
+import React from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+export default function AlertEx() {
+  const myAlertFn = () => {
+    Alert.alert("title", "message", [
+      { text: "Yes", onPress: () => console.log("Pressed yes") },
+      { text: "No", onPress: () => console.log("Pressed no") },
+    ]);
+  };
+
+  const touchablePressed = () => {
+    console.log("pressed the touchable")
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Good Morning</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+      {/* You can use self closing syntax when you arent putting anything inside */}
+      <Button title="Alert Button" onPress={myAlertFn} />
+      <TouchableHighlight
+        onPress={touchablePressed}
+        underlayColor="hotpink"
+      >
+        <Text>Touchable Highlight</Text>
+      </TouchableHighlight>
+      <TouchableOpacity style={styles.opacityContainer}>
+        <Text style={styles.text}>Touchable Opacity</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  opacityContainer: {
+    height: 40,
+    width: "50%",
+    backgroundColor: "yellow",
+    borderWidth: 2,
+    borderColor: "orange",
+  },    
+  text: {
+    fontWeight: "bold",
   },
 });
+

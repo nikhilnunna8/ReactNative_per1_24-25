b0036038e4eb806d97e5aa6dd39387688b8cebbb
nunna8021 <nikhil.nunna9@gmail.com>
2025-01-27 08:08:56 -0600

    Updated assignmentTwo
    Set up styles

--- assignmentTwo/app/index.tsx
+++ assignmentTwo/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hi</Text>
+    <View style={styles.background}>
+      <Text style={styles.title}>Cars Info</Text>
+      
     </View>
   );
 }
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  background: {
+    backgroundColor: "white",
+    justifyContent: "flex-start",
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+});
